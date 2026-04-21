c653f232f5343823ffa27eecb404b31588e1f3a8
sushil811 <sushilkumarprajapati689@gmail.com>
2026-04-21 16:39:00 +0530

    Configure for Render deployment

--- server/index.js
+++ server/index.js
@@ -452,5 +452,13 @@ app.get('/api/attendance/dashboard-summary/:userId', async (req, res) => {
     }
 });
 
+// Serve Static Assets in Production
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/dist')));
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, '../client', 'dist', 'index.html'));
+    });
+}
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
